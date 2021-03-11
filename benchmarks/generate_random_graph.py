c62575848b6c2aa755c42f09d0067dc8c9ff4039
Nicolas Rühling <nicolas.ruehling@uni-potsdam.de>
2021-03-11 11:20:26 +0100

    script for running benchmarks

--- benchmarks/generate_random_graph.py
+++ benchmarks/generate_random_graph.py
@@ -2,13 +2,13 @@ import argparse
 import random
 import os
 
-parser = argparse.ArgumentParser(
-    description=
-    'Generate random graph with n nodes and probability p to generate an edge.'
-)
-parser.add_argument('--num_nodes', '-n', type=int, help='Number of nodes')
+# parser = argparse.ArgumentParser(
+#     description=
+#     'Generate random graph with n nodes and probability p to generate an edge.'
+# )
+# parser.add_argument('--num_nodes', '-n', type=int, help='Number of nodes')
 
-args = parser.parse_args()
+# args = parser.parse_args()
 
 
 def generate_graph(num_nodes, prob):
@@ -23,14 +23,21 @@ def generate_graph(num_nodes, prob):
 
 
 if __name__ == '__main__':
-    random.seed(31)
+    # random.seed(31)
     outdir = 'benchmarks/random_graphs'
 
     probs = [0.5, 0.8, 0.9, 1.0]
+    nodes = [10, 20, 50, 100, 200, 500, 1000]
+    for n in nodes:
+        for p in probs:
+            program = generate_graph(n, p)
+            filename = f'N{n}_p{int(p*100)}.lp'
+            outpath = os.path.join(outdir, filename)
+            counter = 2
+            while os.path.exists(outpath):
+                filename = f'N{n}_p{int(p*100)}_{counter}.lp'
+                outpath = os.path.join(outdir, filename)
+                counter += 1
 
-    for p in probs:
-        program = generate_graph(args.num_nodes, p)
-        filename = f'N{args.num_nodes}_p{int(p*100)}.lp'
-
-        with open(os.path.join(outdir, filename), 'w') as f:
-            f.write('\n'.join(program))
+            with open(outpath, 'w') as f:
+                f.write('\n'.join(program))
