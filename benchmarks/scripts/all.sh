0a46ea569c1f749f7504194e85f8aad9f810dc5e
Nicolas Rühling <82144991+nrueh@users.noreply.github.com>
2023-03-30 13:09:10 +0200

    Update all.sh

--- benchmarks/scripts/all.sh
+++ benchmarks/scripts/all.sh
@@ -6,9 +6,9 @@
 #./scripts/run_bm.sh bm plingo-problog grid exact '--frontend=problog'
 
 # Other P-Log benchmarks
-#./scripts/run_bm.sh bm plingo-problog nasa exact '--frontend=plog'
-#./scripts/run_bm.sh bm plingo-problog squirrel exact '--frontend=plog'
-#./scripts/run_bm.sh bm plingo-problog blocks exact '--frontend=plog'
+./scripts/run_bm.sh bm plingo-problog nasa exact '--frontend=plog'
+./scripts/run_bm.sh bm plingo-problog squirrel exact '--frontend=plog'
+./scripts/run_bm.sh bm plingo-problog blocks exact '--frontend=plog'
 
 # LPMLN benchmarks
 ./scripts/run_bm.sh bm plingo-problog alzheimer_problog mpe '--frontend=lpmln-alt'
